83dc978bd03dbd8ee1c38c8c46e6a67db6138d24
Josh Tucholski <josh@techelevator.com>
2018-03-05 21:34:12 -0500

    Added controllers part 2.

---
+++ m3-w8d2-controllers-part2/FormsWithHttpPost/DAL/ReviewSqlDAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FormsWithHttpPost.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace FormsWithHttpPost.DAL
+{
+    public class ReviewSqlDAL : IReviewDAL
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["squirrelReviews"].ConnectionString;
+
+        public List<Review> GetAllReviews()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveReview(Review newReview)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ m3-w8d2-controllers-part2/FormsWithHttpPost/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using FormsWithHttpPost.DAL;
+using Ninject;
+using Ninject.Web.Common.WebHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FormsWithHttpPost
+{
+    public class MvcApplication : NinjectHttpApplication
+    {
+
+
+        protected override void OnApplicationStarted()
+        {
+            base.OnApplicationStarted();
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+
+            kernel.Bind<IReviewDAL>().To<ReviewSqlDAL>();
+
+            return kernel;
+        }
+    }
+}
