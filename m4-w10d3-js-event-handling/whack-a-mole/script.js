9a9983c95d356b4024292f8a3945b4b27348c45c
Josh Tucholski <josh@techelevator.com>
2018-03-21 06:46:17 -0400

    Added start of javascript exercises.

---
+++ m4-w10d3-js-event-handling/whack-a-mole/script.js
@@ -0,0 +1,23 @@
+// 1. When the peek button is clicked a mole appears in a random dirt mmound
+function peek() { 
+
+    // Get all of the dirt mounds
+
+    // Pick one at random
+
+    // Add the .up class to it
+
+}
+
+
+//3. When a mole is bonked, increment the score
+function bonk() { 
+
+    // Get the score
+    
+    // Increment the score
+
+    // Make the mole disappear
+    
+}
+
